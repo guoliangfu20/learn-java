851d47d065a592f58bcd3b273f8389a4b837dcee
fuguoliang <fglone@126.com>
2023-06-26 17:28:19 +0800

    1

---
+++ nacosTest/src/main/java/com/learns/NacosTestApplication.java
@@ -0,0 +1,15 @@
+package com.learns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosTestApplication.class, args);
+    }
+
+}
---
+++ nacosTest/src/test/java/com/learns/NacosTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
